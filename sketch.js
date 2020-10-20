6490fa02760c8b0d5f0d5ccfc79e5e6e8882cffa
Shikha2889 <shikhachaudhary.28@gmail.com>
2020-10-20 19:19:02 +0530

    created Box7

--- sketch.js
+++ sketch.js
@@ -37,9 +37,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    box7 = new Box(100,100,40,50);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+   // log6 = new Log(230,180,80, PI/2);
+    slingShot = new SlingShot(bird.body,{x:200,y:50});
 }
 
 function draw(){
@@ -63,6 +64,15 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+   // log6.display();
+    slingShot.display(); 
+    box7.display();   
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+    slingShot.fly();
 }
